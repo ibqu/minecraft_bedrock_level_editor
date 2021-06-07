6631a17da45b597e8a141df3ee31c4844eccddeb
Brian Quach <bqua694@Uoa.auckland.ac.nz>
2021-06-07 17:30:49 +1200

    Create event listeners for the buttons

---
+++ script.js
@@ -0,0 +1,17 @@
+/*Steps:
+Initialise the event listeners for the buttons
+Write the onload event listener to add the event listeners to the buttons
+*/
+
+function open_file(){
+
+}
+
+function save_file(){
+
+}
+
+onload = function(){
+    document.getElementById("open_file").onclick = open_file;
+    document.getElementById("save_file").onclick = save_file;
+}
